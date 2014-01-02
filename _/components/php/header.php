cd055e4e2b934c0ca6305a5c8d6e1c0241f804c2
shoutingwords <shoutingwords@gmail.com>
2014-01-03 00:41:14 +0530

    fix small logo display

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -37,7 +37,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                       </button>
-                      <a class="navbar-brand" href="index.php"><img src="images/logo-small.png" alt="ShoutingWords Logo" class="visible-md visible-xs"></a>
+                      <a class="navbar-brand" href="index.php"><img src="images/logo-small.png" alt="ShoutingWords Logo" class="visible-sm visible-xs"></a>
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
