de71b6ca1b3943abd266c1567afe77b29c535c36
shoutingwords <shoutingwords@gmail.com>
2014-01-02 21:51:13 +0530

    Revert changes done in sidebar authorbox

--- _/components/php/sidebar-widget-authorbox.php
+++ _/components/php/sidebar-widget-authorbox.php
@@ -1,11 +1,11 @@
 	          	<aside class="media author-box widget-box" id="author-widget">
                 <div class="row">
-                  <div class="col col-lg-2 col-md-2 col-sm-3">
+                  <div class="col col-lg-3 col-md-3 col-sm-3">
     	            	<a class="pull-left" href="#">
     	                <img class="media-object author-image img-thumbnail img-responsive" src="images/author-tn.jpg" alt="About the Author">
     	              </a>
                   </div><!-- /col -->
-                  <div class="col col-lg-10 col-md-10 col-sm-9">
+                  <div class="col col-lg-9 col-md-9 col-sm-9">
     	              <div class="media-body">
     	                <h4 class="media-heading author-name">About the Author</h4>
     	                <p class="author-bio small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
